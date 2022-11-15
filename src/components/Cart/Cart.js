9c9c19175d60166b051261d076beee1b65a46e48
jahidhasan6484 <jahidhasan6484@gmail.com>
2022-11-15 14:44:08 +0600

    shopping cart done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,15 +5,47 @@ const Cart = () => {
     const [cart, setCart, productsItems] = useContext(CartContext);
 
     const cartTotal = () => {
-        return cart.reduce(function (acc, obj) { return acc + obj.price * obj.quantity; }, 0);
+        return cart.reduce(function (acc, obj) { 
+            return acc + obj.price * obj.quantity; 
+        }, 0);
     }
 
     const itemIncrease = (_id) => {
-        console.log(_id)
+        cart.map((item, index) => {
+            if (item.id == _id) {
+                const update = {
+                    ...item,
+                    quantity: item.quantity + 1
+                }
+                let tempCart = [...cart]
+                tempCart[index] = update;
+                localStorage.setItem("cart", JSON.stringify(tempCart))
+
+                setCart(tempCart)
+            }
+        })
     }
 
     const itemDecrease = (_id) => {
-        console.log(_id)
+        cart.map((item, index) => {
+            if (item.id == _id) {
+                const update = {
+                    ...item,
+                    quantity: item.quantity - 1
+                }
+                let tempCart = [...cart]
+                tempCart[index] = update;
+                if (tempCart[index].quantity < 1) {
+
+                    const updatedCart = tempCart.filter((item) => item.id !== _id)
+                    localStorage.setItem("cart", JSON.stringify(updatedCart))
+                    setCart(updatedCart)
+                } else {
+                    localStorage.setItem("cart", JSON.stringify(tempCart))
+                    setCart(tempCart)
+                }
+            }
+        })
     }
 
     return (
@@ -23,10 +55,10 @@ const Cart = () => {
             {
                 cart.length > 0 &&
                 <div className="row">
-                    <p className="col-md-2 col-2 first">Name</p>
-                    <p className="col-md-2 col-4 second">Price</p>
-                    <p className="col-md-5 col-4 third">Quantity</p>
-                    <p className="col-md-3 col-2 fourth">Total</p>
+                    <p className="col-md-3 col-3 first">Name</p>
+                    <p className="col-md-3 col-3 second">Price</p>
+                    <p className="col-md-3 col-3 third">Quantity</p>
+                    <p className="col-md-3 col-3 fourth">Total</p>
                 </div>
             }
 
@@ -36,9 +68,9 @@ const Cart = () => {
                         cart.map((cartItem) => {
                             return (
                                 <div className="row" key={cartItem.id}>
-                                    <p className="col-md-2 col-2 first">{cartItem.name}</p>
-                                    <p className="col-md-2 col-4 second">{cartItem.price}</p>
-                                    <p className="col-md-5 col-4 third">
+                                    <p className="col-md-3 col-3 first">{cartItem.name}</p>
+                                    <p className="col-md-3 col-3 second">৳ {cartItem.price}</p>
+                                    <p className="col-md-3 col-3 third quantity_content">
                                         <svg onClick={() => itemDecrease(cartItem.id)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash-lg" viewBox="0 0 16 16">
                                             <path fillRule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z" />
                                         </svg>
@@ -47,12 +79,12 @@ const Cart = () => {
                                             <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
                                         </svg>
                                     </p>
-                                    <p className="col-md-3 col-2 fourth">{cartItem.price * cartItem.quantity}</p>
+                                    <p className="col-md-3 col-3 fourth">৳ {cartItem.price * cartItem.quantity}</p>
                                 </div>
                             )
                         })
                     }
-                    <p className="total">Total Payable : {cartTotal()}</p>
+                    <p className="total">Total Payable : ৳ {cartTotal()}</p>
                 </> : "Cart is empty"
             }
         </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -21,25 +21,9 @@ const Navbar = () => {
                         <li className="nav-item">
                             <Link to="/" className="nav-link active" aria-current="page" href="#">Home</Link>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled">Disabled</a>
-                        </li>
                     </ul>
                     <div className="d-flex gap-2">
-                        <button className='btn btn-outline-dark'>{cartTotal()} taka</button>
+                        <button className='btn btn-outline-dark'>৳ {cartTotal()} taka</button>
                         <Link to="/cart">
                             <button className="btn btn-outline-success" type="submit">Cart {cart.length}</button>
                         </Link>
